70ce5b99dc764cdc12f7021f6508097f332bd650
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-11-08 20:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+const ol=document.getElementById("todoList");
+const btn=document.getElementById("addTodoBtn");
+const input=document.getElementById("input");
 
+btn.addEventListener("click",()=>{
+	const li=document.createElement("li");
+	li.innerText=input.value;
+	ol.appendChild(li);
+	input.value=""
+})
