a81ae8111dd979692b70171fb9f493659d7fe788
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-11-08 20:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 //your code here
-const ol=document.getElementById("todoList");
-const btn=document.getElementById("addTodoBtn");
-const input=document.getElementById("newTodoInput");
-
-btn.addEventListener("click",()=>{
-	const li=document.createElement("li");
-	li.innerText=input.value;
-	ol.appendChild(li);
-	input.value=""
-})
